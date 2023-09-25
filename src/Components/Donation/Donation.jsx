6c1b39785a43117a2fff8588d59b7667af679b8c
Ariful2634 <80531678+Ariful2634@users.noreply.github.com>
2023-09-25 23:48:27 +0600

    showing see all button when donation cards are more than four

--- src/Components/Donation/Donation.jsx
+++ src/Components/Donation/Donation.jsx
@@ -5,40 +5,43 @@ import DonationDetails from "./DonationDetails";
 const Donation = () => {
 
 
-    const [donation,setDonation]=useState([])
-    const [noDonation,setNoDonation]=useState("")
-    const [show,setShow]=useState(false)
+    const [donation, setDonation] = useState([])
+    const [noDonation, setNoDonation] = useState("")
+    const [show, setShow] = useState(4)
 
-    
-    useEffect(()=>{
-        
+
+    useEffect(() => {
+
         const item = JSON.parse(localStorage.getItem('donation'))
-       if(item){
-        setDonation(item)
-       }
-       else{
-        
-        setNoDonation("There is no donation yet")
-       }
-    },[])
+        if (item) {
+            setDonation(item)
+        }
+        else {
+
+            setNoDonation("There is no donation yet")
+        }
+    }, [])
+    console.log(donation.length)
 
 
     return (
         <div>
-          
-            
-           { noDonation ? <p className="text-5xl text-center mt-20">There is no donation yet</p> :
-            <div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 max-w-6xl mx-auto gap-8 mt-8">
-                    {
-                       show ?  donation.map(donate=><DonationDetails key={donate.id} donate={donate}></DonationDetails>) :
-                       donation.slice(0,4).map(donate=><DonationDetails key={donate.id} donate={donate}></DonationDetails>)
-                    }
+
+
+            {noDonation ? <p className="text-5xl text-center mt-20">There is no donation yet</p> :
+                <div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 max-w-6xl mx-auto gap-8 mt-8">
+                        {
+
+                            donation.slice(0, show).map(donate => <DonationDetails key={donate.id} donate={donate}></DonationDetails>)
+                        }
+                    </div>
+                    <div className={show>=donation.length ? 'hidden' : ''}>
+                        <div className="text-center mt-6">
+                            <button onClick={() => setShow(donation.length)} className="btn bg-[#009444] text-white">See All</button>
+                        </div>
+                    </div>
                 </div>
-                <div className="text-center mt-6">
-                    <button onClick={()=>setShow(!show)} className="btn bg-[#009444] text-white">See All</button>
-                </div>
-            </div>
             }
         </div>
     );
